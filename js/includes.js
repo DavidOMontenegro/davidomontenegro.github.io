2fe5928d0bbdbccd34399b0b2f5967a18aa4faad
DavidOMontenegro <davidom.official@gmail.com>
2025-10-31 22:26:06 -0300

    Fix HTML to accept imports

--- js/includes.js
+++ js/includes.js
@@ -1,28 +1,17 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-export function loadHTML(selector, filePath) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const element = document.querySelector(selector);
-        if (!element) {
-            console.error(`loadHTML: No element found for selector ${selector}.`);
-            return;
+export async function loadHTML(selector, filePath) {
+    const element = document.querySelector(selector);
+    if (!element) {
+        console.error(`loadHTML: No element found for selector ${selector}.`);
+        return;
+    }
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+            throw new Error(`Failed to load ${filePath} (${response.status})`);
         }
-        try {
-            const response = yield fetch(filePath);
-            if (!response.ok) {
-                throw new Error(`Failed to load ${filePath} (${response.status})`);
-            }
-            element.innerHTML = yield response.text();
-        }
-        catch (err) {
-            console.error(err);
-        }
-    });
+        element.innerHTML = await response.text();
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { loadHTML } from './includes.js';
-function init() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const depth = (window.location.pathname.match(/\//g) || []).length - 1;
-        const prefix = depth > 1 ? '../'.repeat(depth - 1) : './';
-        yield Promise.all([
-            loadHTML('#header', `${prefix}components/header.html`),
-            loadHTML('#footer', `${prefix}components/footer.html`),
-        ]);
-        if (document.head) {
-            try {
-                const res = yield fetch(`${prefix}components/head.html`);
-                if (res.ok) {
-                    const html = yield res.text();
-                    document.head.insertAdjacentHTML('beforeend', html);
-                }
-            }
-            catch (err) {
-                console.error(err);
+async function init() {
+    const depth = (window.location.pathname.match(/\//g) || []).length - 1;
+    const prefix = depth > 1 ? '../'.repeat(depth - 1) : './';
+    await Promise.all([
+        loadHTML('#header', `${prefix}components/header.html`),
+        loadHTML('#footer', `${prefix}components/footer.html`),
+    ]);
+    if (document.head) {
+        try {
+            const res = await fetch(`${prefix}components/head.html`);
+            if (res.ok) {
+                const html = await res.text();
+                document.head.insertAdjacentHTML('beforeend', html);
             }
         }
-        console.log('Common layout loaded.');
-    });
+        catch (err) {
+            console.error(err);
+        }
+    }
+    console.log('Common layout loaded.');
 }
 init();
